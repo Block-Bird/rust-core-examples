a69b838fbbc9ed1310072113120dad4a736d29cb
Asad Ali <asad.sial.4142@gmail.com>
2023-01-04 15:57:30 +0500

    traits and impl prac

---
+++ traitAndStructs/target/debug/deps/traitAndStructs-bc317fd561623237.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/traitAndStructs/target/debug/deps/traitAndStructs-bc317fd561623237: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/traitAndStructs/target/debug/deps/traitAndStructs-bc317fd561623237.d: src/main.rs
+
+src/main.rs:
---
+++ traitAndStructs/target/debug/traitAndStructs.d
@@ -0,0 +1,1 @@
+/Users/asadali/Rust-Projects/rust-examples/traitAndStructs/target/debug/traitAndStructs: /Users/asadali/Rust-Projects/rust-examples/traitAndStructs/src/main.rs
