a2064cad14df4ac7f8b67cce68ac8ffee6375ba9
Asad Ali <asad.sial.4142@gmail.com>
2023-01-16 17:20:58 +0500

    cons

--- box-example/src/main.rs
+++ box-example/src/main.rs
@@ -1,4 +1,19 @@
-fn main() {
-    let b = Box::new(5);
-    println!("B is {}", *b);
+// The basic example of Box 
+// fn main() {
+//     let b = Box::new(5);
+//     println!("B is {}", *b);
+// }
+
+
+
+// The Main use case for box(recursive)
+// Use Vec heap data structure instead of that complex shit 
+enum List{
+    Cons(i32, List), 
+    Nil, 
+}
+
+use List::{Cons, Nil};
+fn main () {
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
 }
