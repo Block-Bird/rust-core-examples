1aeede03354a1e23f15588b001fd7634437723ae
Asad Ali <asad.sial.4142@gmail.com>
2023-04-20 19:15:07 +0500

    binary code push

--- macros-repeat/src/main.rs
+++ macros-repeat/src/main.rs
@@ -5,13 +5,28 @@ macro_rules! find_min {
     }
 }
 
-fn main() {
-    println!("{}", find_min!(156487, 9, 7968,6578,7986,567));
-    let result = add(2, 3);
-    println!("Result: {}", result);
 
-    let result = sub(5, 2);
-    println!("Result: {}", result);
+macro_rules! print_binary {
+    ($val:expr) => {
+        println!("{:#b}", $val);
+    };
+}
+
+
+fn main() {
+    // println!("{}", find_min!(156487, 9, 7968,6578,7986,567));
+    // let result = add(2, 3);
+    // println!("Result: {}", result);
+
+    // let result = sub(5, 2);
+    // println!("Result: {}", result);
+
+    let num: u8 = 42;
+    print_binary!(num);
+
+    let number: u8 = 77; 
+
+    println!("Binary of 77 is {:#b} ", number);
 
 
 }
@@ -34,5 +49,8 @@ macro_rules! sub_func {
         }
     );
 }
+
+
+
 
 sub_func!();
