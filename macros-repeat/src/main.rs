37796daf986d7f0d4e8bff23b5eb34604db6ed57
Asad Ali <asad.sial.4142@gmail.com>
2023-04-21 16:09:33 +0500

    simple example

--- macros-repeat/src/main.rs
+++ macros-repeat/src/main.rs
@@ -1,8 +1,55 @@
+use std::fmt::Write;
+
+#[derive(Debug)]
+struct Block {
+    index: u32,
+    timestamp: u64,
+    data: String,
+    previous_hash: String,
+    hash: String,
+}
+
 macro_rules! find_min {
     ($x: expr) => ($x); 
     ($x: expr, $($y: expr), +) => {
         std::cmp::min($x, find_min!($($y), + ))
     }
+}
+
+fn calculate_hash(block: &Block) -> String {
+    let mut s = String::new();
+    write!(&mut s, "{}{}{}{}", block.index, block.timestamp, block.data, block.previous_hash).unwrap();
+    s
+}
+
+fn generate_block(previous_block: &Block, data: String) -> Block {
+    let index = previous_block.index + 1;
+    let timestamp = 0; // TODO: implement timestamp
+    let previous_hash = previous_block.hash.clone();
+    let hash = calculate_hash(&Block { index, timestamp, data: data.clone(), previous_hash: previous_hash.clone(), hash: String::new() });
+    Block { index, timestamp, data, previous_hash, hash }
+}
+
+fn is_valid_new_block(new_block: &Block, previous_block: &Block) -> bool {
+    if previous_block.index + 1 != new_block.index {
+        return false;
+    }
+    if calculate_hash(previous_block) != new_block.previous_hash {
+        return false;
+    }
+    if calculate_hash(new_block) != new_block.hash {
+        return false;
+    }
+    true
+}
+
+fn is_valid_chain(chain: &[Block]) -> bool {
+    for i in 1..chain.len() {
+        if !is_valid_new_block(&chain[i], &chain[i - 1]) {
+            return false;
+        }
+    }
+    true
 }
 
 
@@ -27,6 +74,25 @@ fn main() {
     let number: u8 = 77; 
 
     println!("Binary of 77 is {:#b} ", number);
+    let mut blockchain = vec![Block {
+        index: 0,
+        timestamp: 0,
+        data: String::from("Genesis block"),
+        previous_hash: String::new(),
+        hash: String::new(),
+    }];
+
+    blockchain.push(generate_block(&blockchain[0], String::from("First block")));
+    blockchain.push(generate_block(&blockchain[1], String::from("Second block")));
+
+    println!("Blockchain: {:?}", blockchain);
+
+    if is_valid_chain(&blockchain) {
+        println!("Blockchain is valid");
+    } else {
+        println!("Blockchain is not valid");
+    }
+
 
 
 }
