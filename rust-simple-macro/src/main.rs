692b84b2e499dd175ccfdccf012cccb74bc1cdb5
Asad Ali <asad.sial.4142@gmail.com>
2023-04-22 15:08:01 +0500

    nexted updation

--- rust-simple-macro/src/main.rs
+++ rust-simple-macro/src/main.rs
@@ -15,5 +15,5 @@ macro_rules! print_nested {
 
 fn main() {
     let nested_list = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
-    print_nested!(nested_list);
+    // print_nested!(nested_list);
 }
