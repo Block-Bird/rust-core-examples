e811cf8bdacefe7a14ccacd097cc1667450ef73c
Asad Ali <asad.sial.4142@gmail.com>
2023-04-27 19:47:42 +0500

    micro services

---
+++ micro-services/src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello, {}!", name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
---
+++ micro-services/target/debug/deps/micro_services-d013eb0b09c0ee74.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/micro-services/target/debug/deps/micro_services-d013eb0b09c0ee74.rmeta: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/micro-services/target/debug/deps/micro_services-d013eb0b09c0ee74.d: src/main.rs
+
+src/main.rs:
