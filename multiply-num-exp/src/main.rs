553c37e97b38a134e77f5bbbdce2a9bdcb38628f
Asad Ali <asad.sial.4142@gmail.com>
2023-05-03 18:34:28 +0500

    sorting bug

---
+++ multiply-num-exp/src/main.rs
@@ -0,0 +1,22 @@
+macro_rules! multiply {
+    // The base case for multiplying one number by another is simply to
+    // return their product.
+    ($x:expr , $y:expr) => {{
+        $x * $y
+    }};
+    // For multiplying more than two numbers, we recursively call the
+    // macro with the first two numbers, then multiply their result by
+    // the remaining numbers.
+    ($x:expr,* $($y:expr)*) => {{
+        multiply!($x * multiply!($($y)*))
+    }};
+}
+
+fn main() {
+    let x = 2;
+    let y = 3;
+    let z = 4;
+
+    let result = multiply!(x * y * z);
+    println!("{}", result); // prints "24"
+}
---
+++ multiply-num-exp/target/debug/deps/multiply_num_exp-5db6c6c1a055921a.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/multiply-num-exp/target/debug/deps/multiply_num_exp-5db6c6c1a055921a: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/multiply-num-exp/target/debug/deps/multiply_num_exp-5db6c6c1a055921a.d: src/main.rs
+
+src/main.rs:
