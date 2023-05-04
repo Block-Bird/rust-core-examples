79bdc0c7b400b08997d0114e0d2485c51271cafb
Asad Ali <asad.sial.4142@gmail.com>
2023-05-04 14:16:57 +0500

    move keyword example

---
+++ move-example/target/debug/deps/move_example-b2b892ca8c10ce5a.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/move-example/target/debug/deps/move_example-b2b892ca8c10ce5a: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/move-example/target/debug/deps/move_example-b2b892ca8c10ce5a.d: src/main.rs
+
+src/main.rs:
---
+++ move-example/target/debug/move-example.d
@@ -0,0 +1,1 @@
+/Users/asadali/Rust-Projects/rust-examples/move-example/target/debug/move-example: /Users/asadali/Rust-Projects/rust-examples/move-example/src/main.rs
