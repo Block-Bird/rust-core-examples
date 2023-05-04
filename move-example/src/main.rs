40a0495a4628cd8b6253a872430af5931b0922a0
Asad Ali <asad.sial.4142@gmail.com>
2023-05-04 14:24:56 +0500

    example 2 of move

--- move-example/src/main.rs
+++ move-example/src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
+
     let x = vec![1, 2, 3]; 
     let closure = move || println!("This is {:?}", x);
-    closure(); 
+    // closure(); 
+    // println!("X : {:?}", x); 
+
+
+
+    // Example 2
+    let x = 5; 
+    let closure = move || {
+        let result = &x; 
+        println!("Result: {}", result);
+    }; 
+
+    // closure(); 
+    println!(" X is shaa  {:?} ", x);
 }
