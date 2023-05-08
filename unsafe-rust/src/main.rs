9c5715996ea31376bceb6770ab059318754c5d09
Asad Ali <asad.sial.4142@gmail.com>
2023-05-08 21:08:23 +0500

    Adding Unsafe

--- unsafe-rust/src/main.rs
+++ unsafe-rust/src/main.rs
@@ -5,7 +5,13 @@ fn main() {
     let r1 = &num as *const i32;
     let r2 = &mut num as *mut i32;
     let r3 = &num;  
-    println!("R1 is {:?}", *r1); 
+    
+
+    // Dereferrencing Raw pointer in Unsafe
+    unsafe {
+        println!("R1 is {:?}", *r1); 
+    }
+    
     println!("R2 is {:?}", r2); 
     println!("R3 is {:?}", *r3); 
 
