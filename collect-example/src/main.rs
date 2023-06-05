8c1c2e000ea7b80ed0617e999f6fc89e7d81d0e6
Asad Ali <asad.sial.4142@gmail.com>
2023-06-05 23:41:33 +0500

    iter and collected example

--- collect-example/src/main.rs
+++ collect-example/src/main.rs
@@ -2,12 +2,19 @@
 
 fn main()
 {
-static  A : [i32; 3] = [1, 2, 3];
+// static  A : [i32; 3] = [1, 2, 3];
 
-let doubled : Vec<i32 > = A.iter().map(|&x| x*2 ).collect();
+// let doubled : Vec<i32 > = A.iter().map(|&x| x*2 ).collect();
 
-assert_eq!(vec![2,4,6], doubled);
+// assert_eq!(vec![2,4,6], doubled);
 
-println!("{:?}", doubled); 
+// println!("{:?}", doubled); 
+
+
+let numbers = vec![1, 2, 3, 4 ]; 
+let squared_numbers= numbers.iter().map(|x| x*x); 
+println!("Here's squared Number {:?}", squared_numbers);
+let collected_number : Vec<i32>  = squared_numbers.collect(); 
+println!("Here're Collected Numbers {:?}", collected_number); 
 
 }
