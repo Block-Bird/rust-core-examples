d0f1e7b63a0ade626b9752c3b2f8198a8f4a9eaa
Asad Ali <asad.sial.4142@gmail.com>
2023-09-15 17:52:00 +0500

    life-time

---
+++ life-time-annotation/target/debug/deps/life_time_annotation-5166095de2bd72f3.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/life-time-annotation/target/debug/deps/life_time_annotation-5166095de2bd72f3.rmeta: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/life-time-annotation/target/debug/deps/life_time_annotation-5166095de2bd72f3.d: src/main.rs
+
+src/main.rs:
---
+++ life-time-annotation/target/debug/deps/life_time_annotation-9721f5298036e647.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/life-time-annotation/target/debug/deps/life_time_annotation-9721f5298036e647.rmeta: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/life-time-annotation/target/debug/deps/life_time_annotation-9721f5298036e647.d: src/main.rs
+
+src/main.rs:
