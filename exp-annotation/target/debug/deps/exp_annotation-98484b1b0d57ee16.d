6f5f771cb2bed37d5cb8a419c8be0545c416ed0a
Asad Ali <asad.sial.4142@gmail.com>
2023-09-18 22:28:49 +0500

    annotation exp

---
+++ exp-annotation/target/debug/deps/exp_annotation-98484b1b0d57ee16.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/exp-annotation/target/debug/deps/exp_annotation-98484b1b0d57ee16.rmeta: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/exp-annotation/target/debug/deps/exp_annotation-98484b1b0d57ee16.d: src/main.rs
+
+src/main.rs:
---
+++ exp-annotation/target/debug/deps/exp_annotation-e9e251932f410123.d
@@ -0,0 +1,5 @@
+/Users/asadali/Rust-Projects/rust-examples/exp-annotation/target/debug/deps/exp_annotation-e9e251932f410123.rmeta: src/main.rs
+
+/Users/asadali/Rust-Projects/rust-examples/exp-annotation/target/debug/deps/exp_annotation-e9e251932f410123.d: src/main.rs
+
+src/main.rs:
