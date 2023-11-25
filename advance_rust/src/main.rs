c5cbc1e83a9ed1fdb0bba267d5e284301a7706d4
Asad Ali <asad.sial.4142@gmail.com>
2023-11-25 23:04:54 +0500

    procedural macros

---
+++ advance_rust/src/main.rs
@@ -0,0 +1,21 @@
+// Procedural macro example
+use proc_macro;
+
+#[proc_macro]
+pub fn generate_function(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // Input is the code inside the macro invocation
+    let input_str = input.to_string();
+
+    // Generating new code
+    let output = format!("fn generated_function() {{ println!(\"Generated: {}\"); }}", input_str);
+
+    // Convert the generated code back into a TokenStream
+    output.parse().unwrap()
+}
+// Using the procedural macro
+generate_function!(Hello, world!);
+
+fn main() {
+    // Calling the generated function
+    generated_function();
+}
